625f7a6617ae297e613ab572b371109b4bbe79a5
Cleuvin-Dev <cleuvin.dev@gmail.com>
2025-03-24 13:01:43 -0300

    Criando a tela do editor de texto

---
+++ Editor.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Text;
+
+namespace EditorHTML
+{
+  public static class Editor
+  {
+    public static void Show()
+    {
+      Console.Clear();
+      Console.BackgroundColor = ConsoleColor.White;
+      Console.ForegroundColor = ConsoleColor.Black;
+      Console.Clear();
+      Console.WriteLine("MODO EDITOR");
+      Console.WriteLine("-----------");
+      Start();
+    }
+
+    public static void Start()
+    {
+
+      var file = new StringBuilder();
+
+      do
+      {
+        file.Append(Console.ReadLine()); // Adiciona o texto na tela
+        file.Append(Environment.NewLine);
+
+      } while (Console.ReadKey().Key != ConsoleKey.Escape); // Enquanto o usuario nao digitar a tecla ESC o mesmo permanecerá na tela de edição 
+
+      Console.WriteLine("--------------");
+      Console.WriteLine(" Deseja salvar o arquivo?");
+
+    }
+  }
+}
