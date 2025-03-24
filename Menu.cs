60767111e9fe94d8f01fe061ded44fa8dfe26438
Cleuvin-Dev <cleuvin.dev@gmail.com>
2025-03-24 12:04:55 -0300

    concluido o desenho da tela utilizando o loop for

--- Menu.cs
+++ Menu.cs
@@ -7,8 +7,45 @@ namespace EditorHTML
     public static void Show()
     {
       Console.Clear();
-      Console.BackgroundColor = ConsoleColor.Blue;
-      Console.ForegroundColor = ConsoleColor.Black;
+      Console.BackgroundColor = ConsoleColor.Blue; // deixa fundo do console de azul
+      Console.ForegroundColor = ConsoleColor.Black; // coloque a escrita na cor preta
+
+      DrawScreen(); // Chamando a classe de desenho na tela
+    }
+
+    public static void DrawScreen() // Classe para criar um desenho na tela do usuario
+    {
+      // Desenha a parte superior da tela
+      Console.Write("+");
+      for (int i = 0; i <= 30; i++)
+        Console.Write("-");
+
+      Console.Write("+");
+      Console.Write("\n");
+
+      // Desenha as linhas laterias da tela
+      for (int lines = 0; lines <= 10; lines++)
+      {
+        Console.Write("|");
+        for (int i = 0; i <= 30; i++)
+          Console.Write(" ");
+
+        Console.Write("|");
+        Console.Write("\n");
+      }
+
+      // Desenha a parte inferior da tela
+      Console.Write("+");
+      for (int i = 0; i <= 30; i++)
+        Console.Write("-");
+
+      Console.Write("+");
+      Console.Write("\n");
+
+
+
+
+
     }
   }
 }
